4853079b2fbea7d6220984b4e39d8247b4b6ff2d
Youssef <joeace2002@gmail.com>
2024-11-04 11:27:46 +0000

    Trying to make amount and receipeint visable in post page

---
+++ inc/post.php
@@ -0,0 +1,11 @@
+<header>
+    <h1>Fund Transfer</h1>
+</header>
+<div>
+    Thanks for using this service!
+</div>
+
+<div>
+    You have sent <?= $inputs['amount'] ?> to <?= htmlspecialchars($inputs['recipient_account']) ?> 
+</div>
+
--- index.php
+++ index.php
@@ -4,12 +4,23 @@ session_start();
 
 require __DIR__ .  '/inc/header.php'; 
 
+$errors = []; // for storing the error messages
+$inputs = []; // for storing sanitized input values
+
 $request_method = strtoupper($_SERVER['REQUEST_METHOD']);
 
 if ($request_method == "GET") { 
 
 
     require __DIR__ . "/inc/get.php";
+
+
+}
+
+if ($request_method == "POST") { 
+
+
+    require __DIR__ . "/inc/post.php";
 
 
 }
