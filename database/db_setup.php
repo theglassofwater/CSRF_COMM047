446c7d31eb12984dfaef14e8db75cd250935d931
Youssef <joeace2002@gmail.com>
2024-11-08 21:15:44 +0000

    adding empty database and php db setup

---
+++ database/db_setup.php
@@ -0,0 +1,24 @@
+<?php
+
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('database.sqlite');
+    }
+}
+$db = new MyDB;
+
+// creats users table with password and username ( and id as primary key)
+$db->exec("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+
+// Inserts sammple users with the unique usernames but all with password = "password"
+$password = password_hash('password', PASSWORD_DEFAULT);
+
+
+$db->exec("INSERT INTO users (username, password) VALUES ('user1', '$password')");
+$db->exec("INSERT INTO users (username, password) VALUES ('user2', '$password')");
+$db->exec("INSERT INTO users (username, password) VALUES ('user3', '$password')");
+
+echo "Successfully created Database\n";
+?>
